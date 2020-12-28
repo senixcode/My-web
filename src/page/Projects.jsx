e6b1a0d36c488af111c237dfdb16eb0b05d27302
Senix Code <codesenix@gmail.com>
2020-12-28 14:59:19 -0500

    add card project

--- src/page/Projects.jsx
+++ src/page/Projects.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { TitleCustom } from "../components/atoms/TitleCustom";
+import Project from "../components/organisms/CardCustomProject";
 
 export const Projects = () => {
   return (
     <>
       <TitleCustom menssage="Proyectos" />
+      <Project />
     </>
   );
 };
