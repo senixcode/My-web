5f0db15e40c8e0ec5b011d73446b0dfb5aecc6ba
senixcode <codesenix@gmail.com>
2021-02-08 07:57:59 -0500

    feat: :sparkles: Added the footer with mobile design

---
+++ src/components/organisms/Footer.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { PropStyleTheme } from "../../types";
+import {
+  GithubIconXs,
+  LinkedinIconXs,
+  TelegramIconXs,
+  TwitterIconXs,
+} from "../../image";
+
+const FixedTop = styled.div`
+  position: fixed;
+  width: calc(100vw - ${({ theme }: PropStyleTheme) => theme.sizes.sm});
+  bottom: 1.4em;
+  padding: 0 18px;
+`;
+const NavbarBottom = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 1em;
+  align-items: center;
+  justify-items: center;
+`;
+export const Footer = () => (
+  <FixedTop>
+    <NavbarBottom>
+      <TelegramIconXs />
+      <TwitterIconXs />
+      <GithubIconXs />
+      <LinkedinIconXs />
+    </NavbarBottom>
+  </FixedTop>
+);
