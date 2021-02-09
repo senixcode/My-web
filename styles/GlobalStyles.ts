c40330791fba89665b8ae02a287205e88daed9d8
senixcode <codesenix@gmail.com>
2021-02-08 19:25:49 -0500

    feat: :sparkles: Added global styles

--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -1,6 +1,5 @@
 import {createGlobalStyle} from "styled-components"
-import { ITheme } from "../src/interface/Theme";
-
+import { PropStyleTheme } from "../src/types";
 
 const normalize = `
   /*! modern-normalize | MIT License | https://github.com/sindresorhus/modern-normalize */html{box-sizing:border-box}*,::after,::before{box-sizing:inherit}:root{-moz-tab-size:4;tab-size:4}html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}body{font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'}hr{height:0}abbr[title]{text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:SFMono-Regular,Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{padding:.35em .75em .625em}legend{padding:0}progress{vertical-align:baseline}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}
@@ -9,25 +8,32 @@ const normalize = `
 const GlobalStyles = createGlobalStyle`
 ${normalize};
 body{
-  font-family:${({theme}:{theme:ITheme}) => theme.font}
+  font-family:${({theme}:PropStyleTheme) => theme.font};
+}
+html, body {
+  overflow-x: hidden;
+}
+h1, h2, h3, p{
+  margin:0;
+  font-weight:normal;
+  color:${({theme}:PropStyleTheme) => theme.colors.secondary}
+}
+p{
+  font-weight: lighter;
 }
 h1 {
-  ${({theme}:{theme:ITheme})=> theme.fontSizes.xlarge};
+  ${({theme}:PropStyleTheme)=> theme.fontSizes.xlarge};
 }
 h2 {
-  ${({theme}:{theme:ITheme}) => theme.fontSizes.larger};
+  ${({theme}:PropStyleTheme) => theme.fontSizes.larger};
 }
 h3 {
-  ${({theme}:{theme:ITheme})=> theme.fontSizes.large};
+  ${({theme}:PropStyleTheme)=> theme.fontSizes.large};
 }
 p {
-  ${({theme}:{theme:ITheme}) =>theme.fontSizes.regular};
-  color: ${({theme}:{theme:ITheme}) => theme.colors.textPrimary};
+  ${({theme}:PropStyleTheme) =>theme.fontSizes.regular};
+  color: ${({theme}:PropStyleTheme) => theme.colors.textPrimary};
 }
-@media screen and (min-width:${({theme}:{theme:ITheme}) =>theme.screen.xs}){
-  body{
-    background-color:red;
-  }
-}
+
 `
 export default GlobalStyles;
