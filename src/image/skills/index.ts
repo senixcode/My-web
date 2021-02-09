c8b9dd57087c41c20218ed951884496593d1cc5d
senixcode <codesenix@gmail.com>
2021-02-08 19:22:53 -0500

    feat: :sparkles: Added icon imports

---
+++ src/image/skills/index.ts
@@ -0,0 +1,8 @@
+export {default as TsIcon} from "./ts.svg";
+export {default as ReactIcon} from "./react.svg";
+export {default as VueIcon} from "./vue.svg";
+export {default as NodeIcon}from "./node.svg";
+export {default as GraphqlIcon} from "./graphql.svg";
+export {default as JwtIcon} from "./jwt.svg";
+export {default as MongodbIcon} from "./mongo.svg";
+export {default as DockerIcon} from "./docker.svg";
