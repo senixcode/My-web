7ca4c7968812cc53ea05be84b717d2beb32149b9
senixcode <codesenix@gmail.com>
2021-02-12 14:00:18 -0500

    feat: :sparkles: added function separate camelCase

---
+++ src/helper/separateCamelCase.ts
@@ -0,0 +1,7 @@
+export const separateCamelCase = (word:string):string => {
+    let newWord:string = word.trim()
+                              .split(/(?=[A-Z])/)
+                              .map(word => word.toLowerCase())
+                              .join(' ')
+    return newWord.charAt(0).toUpperCase() + newWord.slice(1);
+}
