3e44be9c6d2d0cb3c2368bff54a30f5cb351143c
senixcode <codesenix@gmail.com>
2021-02-12 13:57:48 -0500

    feat: :sparkles: Added interfaces (section, skill)

---
+++ src/interface/Sections.ts
@@ -0,0 +1,5 @@
+import {FunctionComponent} from "react"
+export interface ISections {
+    name:string;
+    Component:FunctionComponent;
+}
---
+++ src/interface/Skill.ts
@@ -0,0 +1,4 @@
+export interface ISkill {
+    icon:string;
+    name:string;
+} 
