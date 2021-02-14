0f3c6b28c186d83051da8c6a51025c8c6e7307d0
senixcode <codesenix@gmail.com>
2021-02-14 03:16:55 -0500

    fix: :sparkles: rename import Twitter.svg

--- src/image/socialNetworks/index.ts
+++ src/image/socialNetworks/index.ts
@@ -1,4 +1,4 @@
 export {default as TelegramIcon} from "./telegram.svg";
-export {default as TwitterIcon} from "./twitter.svg";
+export {default as TwitterIcon} from "./Twitter.svg";
 export {default as GithubIcon} from "./github.svg";
 export {default as LinkedinIcon} from "./linkedin.svg";
