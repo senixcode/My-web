2720ac70d73335677ed214398073263b3be07f33
senixcode <codesenix@gmail.com>
2021-02-14 02:50:27 -0500

    feat: :sparkles: Added changes for desktop design

--- src/components/sections/ExperienceSection.tsx
+++ src/components/sections/ExperienceSection.tsx
@@ -1,15 +1,19 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 import { Section as SectionGlobal } from "../../../styles/StyleComponentGlobal";
-import { experiences } from '../../cmsFaker/experiences';
-import { ContainerExperience } from '../organisms/ContainerExperience';
+import { experiences } from "../../cmsFaker/experiences";
+import { PropStyleTheme } from "../../types";
+import { ContainerExperience } from "../organisms/ContainerExperience";
 
-const Content = () => (<ContainerExperience experiences={experiences}/>)
 const ExperienceSection = () => (
-  <Section id="about-me">
-    <Content />
+  <Section>
+    <ContainerExperience experiences={experiences}/>
   </Section>
 );
-export default ExperienceSection
+export default ExperienceSection;
 const Section = styled(SectionGlobal)`
   display: grid;
+  @media screen and (min-width: ${(props: PropStyleTheme) =>
+      props.theme.screen.md}) {
+    background: ${(props: PropStyleTheme) => props.theme.colors.primaryVariant};
+  }
 `;
