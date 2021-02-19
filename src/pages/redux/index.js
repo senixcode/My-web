3a625e943b280882b48553e90266535c75b01f0f
senixcode <codesenix@gmail.com>
2021-02-19 14:17:24 -0500

    refactor: :fire: delete file index.js

--- src/pages/redux/index.js
+++
@@ -1,9 +0,0 @@
-// import {systemReducer} from './system/reducers'
-// import {chatReducer} from './chat/reducers'
-
-// const rootReducer = combineReducers({
-//     system: systemReducer,
-//     chat: chatReducer
-// })
-
-// export type RootState = ReturnType<typeof rootReducer>
