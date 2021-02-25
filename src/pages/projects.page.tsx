17296fbd1f13e541c8c6d8afbbbeb9c39396529c
senixcode <codesenix@gmail.com>
2021-02-25 00:33:34 -0500

    feat: :sparkles: Projects with added lenguages switching functionality

--- src/pages/projects.page.tsx
+++ src/pages/projects.page.tsx
@@ -1,10 +1,14 @@
 import { Container } from "../../styles/system/Container";
 import Layout from "../layout";
 import { ProjectsContainer } from "../components/containers/ProjectsContainer";
+import { useRouter } from "next/router";
+import { getProjects } from "../cmsFaker/getProjects";
 export default function Home() {
+    let { locale } = useRouter();
+  let projects = getProjects(locale);
   return (
     <Layout>
-      <ProjectsContainer />
+      <ProjectsContainer projects={projects}/>
     </Layout>
   );
 }
