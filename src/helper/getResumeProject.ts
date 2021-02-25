521b5044e93c5c5235f638e61fca7b333302c6c7
senixcode <codesenix@gmail.com>
2021-02-25 15:09:59 -0500

    feat: :sparkles: Added function to obtain the project summary

---
+++ src/helper/getResumeProject.ts
@@ -0,0 +1,18 @@
+interface IResumeProject {
+  description: string;
+  status: boolean;
+}
+export const getResumeProject = (
+  description: string,
+  limite: number = 130
+): IResumeProject => {
+  let result: IResumeProject = {
+    description: description,
+    status: false,
+  };
+  if (description.length > limite) {
+    result.description = `${description.slice(0, limite)}...`;
+    result.status = true;
+  }
+  return result;
+};
