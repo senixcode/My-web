fcfcc59c61b699d01715810e9764f9c4fdbcdd00
senixcode <codesenix@gmail.com>
2021-03-01 20:06:21 -0500

    feat: :fire: layout deleted

--- src/pages/projects.page.tsx
+++ src/pages/projects.page.tsx
@@ -1,14 +1,8 @@
-import { Container } from "../../styles/system/Container";
-import Layout from "../layout";
 import { ProjectsContainer } from "../components/containers/ProjectsContainer";
 import { useRouter } from "next/router";
 import { getProjects } from "../cmsFaker/getProjects";
 export default function Home() {
-    let { locale } = useRouter();
+  let { locale } = useRouter();
   let projects = getProjects(locale);
-  return (
-    <Layout>
-      <ProjectsContainer projects={projects}/>
-    </Layout>
-  );
+  return <ProjectsContainer projects={projects} />;
 }
