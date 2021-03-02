a3d36c77f2d72f6a2b1f1a541e458e9db89eae07
senixcode <codesenix@gmail.com>
2021-03-01 19:51:59 -0500

    feat: :sparkles: Added next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+export default {
+    description:"About senixcode, portfolio, projects, and ideas",
+    openGraph:{
+        type:'website',
+        locale:'en_US',
+        url:'https://senixcode.dev/',
+        site_name:'About senixcode'
+    },
+    twitter:{
+        handle:'@senixcode',
+        site:'@senixcode',
+        cardType:'summary_large_image'
+    }
+}
