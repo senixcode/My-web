2c9770144c5bb82e8a58a749383f30fa027f7572
senixcode <codesenix@gmail.com>
2021-03-02 04:50:44 -0500

    feat: :sparkles: Added detail container

--- src/pages/detail/[project].page.tsx
+++ src/pages/detail/[project].page.tsx
@@ -1,7 +1,13 @@
 import { useRouter } from "next/router";
+import { Container } from "../../../styles/system/Container";
+import { getProjects, getProjectById, IProjects } from "../../cmsFaker/getProjects";
+import { DetailContainer } from "../../components/containers/DetailContainer";
+
 export default function Detail() {
   const { query } = useRouter();
-  return (
-      <p>{JSON.stringify(query)}</p>
-  );
+  let { locale } = useRouter();
+  let projects = getProjects(locale);
+  const idProject = parseInt(query.project as string);
+  let project:IProjects = getProjectById(projects, idProject);
+  return <DetailContainer {...project}/>
 }
