664eefe8a93fc79caf4f8961d8dd535336555482
senixcode <codesenix@gmail.com>
2021-03-02 19:58:06 -0500

    style: :lipstick: update styles topic

--- src/components/molecules/Topic.tsx
+++ src/components/molecules/Topic.tsx
@@ -9,12 +9,12 @@ export const Topic: FC<{ name: string }> = ({ name }) => (
   </Container>
 );
 const topic = css`
-  padding: 0.3em;
-  /* margin:0.2em; */
-  border-radius: 12px;
+  padding: 0.2em;
+  margin:0.2em;
+  border-radius: 6px;
   border: 2px solid
     ${(props: PropStyleTheme) => props.theme.colors.secondaryVariant};
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 300;
   color: ${(props: PropStyleTheme) => props.theme.colors.textPrimary};
 `;
