f132ba911e3a91d9b158e0185fa78e3a73ab791a
senixcode <codesenix@gmail.com>
2021-03-04 06:32:21 -0500

    feat: :sparkles: feat icon figma

--- src/globalVariables/icons.ts
+++ src/globalVariables/icons.ts
@@ -3,3 +3,4 @@ export const ICON_GITHUB:string = `fab fa-github`
 export const ICON_TELEGRAM:string = `fab fa-telegram-plane`
 export const ICON_TWITTER:string = `fab fa-twitter`
 export const ICON_LINKEDIN:string = `fab fa-linkedin-in`
+export const ICON_FIGMA:string = `fab fa-figma`
