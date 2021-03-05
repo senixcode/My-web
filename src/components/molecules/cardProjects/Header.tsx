357cea6e58b326a6e21dd0467573426c43609285
senixcode <codesenix@gmail.com>
2021-03-04 21:45:59 -0500

    refactor: :recycle: Header separation into components

--- src/components/molecules/cardProjects/Header.tsx
+++ src/components/molecules/cardProjects/Header.tsx
@@ -2,32 +2,15 @@ import styled, { css } from "styled-components";
 import { FC } from "react";
 import { Container } from "../../../../styles/system/Container";
 import { PropStyleTheme } from "../../../types";
-import { Link } from "../../../../styles/system/Link";
-import { ILink, IProjects } from "../../../cmsFaker/projects/interfaces";
+import { IProjects } from "../../../cmsFaker/projects/interfaces";
+import { Icons } from "./IconsHeader";
 
-export const Header: FC<IProjects> = ({ title, links }) => {
-  const Icons = () => {
-    const ContainerImage: FC<ILink> = ({ nameSeo, name, link }) => (
-      <Link title={nameSeo} href={link} fontSizeMd="20px" fontSizeXs="18px">
-        <i className={name}></i>
-      </Link>
-    );
-
-    return (
-      <Container styles={linksContainer} gridColumGap=".3em">
-        {links.map((link, i) => (
-          <ContainerImage key={i} {...link} />
-        ))}
-      </Container>
-    );
-  };
-  return (
-    <Container gridTemplateColumnsXs={`12fr 1fr`} styles={header}>
-      <Title>{title.toUpperCase()}</Title>
-      <Icons />
-    </Container>
-  );
-};
+export const Header: FC<IProjects> = ({ title, links }) => (
+  <Container gridTemplateColumnsXs={`12fr 1fr`} styles={header}>
+    <Title>{title.toUpperCase()}</Title>
+    <Icons links={links} />
+  </Container>
+);
 const Title = styled.h3`
   font-size: 18px;
   font-weight: normal;
@@ -36,7 +19,4 @@ const Title = styled.h3`
 `;
 const header = css`
   width: 100%;
-`;
-const linksContainer = css`
-  display: flex;
 `;
