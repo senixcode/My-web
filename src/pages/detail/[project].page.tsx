caf79e985c19491a5f3ffae7325da7357b6d790a
senixcode <codesenix@gmail.com>
2021-03-04 21:36:42 -0500

    fix: :bug: fix of loading useRouter(query)

--- src/pages/detail/[project].page.tsx
+++ src/pages/detail/[project].page.tsx
@@ -1,30 +1,39 @@
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
-import {
-  getProjects,
-} from "../../cmsFaker/getProjects";
+import { getProjects } from "../../cmsFaker/getProjects";
 import { IProjects } from "../../cmsFaker/projects/interfaces";
 import { getProjectByTitle, seoTitle } from "../../cmsFaker/projects/function";
 import { DetailContainer } from "../../components/containers/DetailContainer";
+import { useEffect, useState } from "react";
 
 export default function Detail() {
-  const { query } = useRouter();
-  let { locale } = useRouter();
-  let projects = getProjects(locale);
-  let project: IProjects = getProjectByTitle(projects, query.project as string);
-  const SEO = {
-    title: seoTitle(project.title, locale),
-    description: project.summary,
-    openGraph: {
-      title: seoTitle(project.title, locale),
-      description: project.summary,
-    },
-  };
+  const { isReady, query, locale } = useRouter();
+  const [project, setProject] = useState<IProjects>();
+  const [SEO, setSEO] = useState({});
+  useEffect(() => {
+    if (isReady) {
+      const projects: Array<IProjects> = getProjects(locale);
+      setProject(getProjectByTitle(projects, query.project as string));
+      project &&
+        setSEO({
+          title: seoTitle(project.title, locale),
+          description: project.summary,
+          openGraph: {
+            title: seoTitle(project.title, locale),
+            description: project.summary,
+          },
+        });
+    }
+  }, [isReady, setProject, setSEO]);
 
   return (
     <>
-      <NextSeo {...SEO} />
-      <DetailContainer {...project} />
+      {isReady && project && SEO && (
+        <>
+          <NextSeo {...SEO} />
+          <DetailContainer {...project} />
+        </>
+      )}
     </>
   );
 }
