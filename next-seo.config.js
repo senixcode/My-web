93b359fc1bdb8469b20d51bfa3d4b3fb32898114
senixcode <codesenix@gmail.com>
2021-03-07 22:44:56 -0500

    refactor: :recycle: change description seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,17 +1,24 @@
 export default {
-    description:"About senixcode, I am a self-taught full stack developer, I will show you my open source projects and ideas.",
-    openGraph:{
-        type:'website',
-        locale:'en_US',
-        url:'https://senixcode.dev/',
-         images: [
-          { url: 'https://raw.githubusercontent.com/senixcode/my-website/main/public/senixcode.jpg' },
-        ],
-        site_name:'About senixcode'
-    },
-    twitter:{
-        handle:'@senixcode',
-        site:'@senixcode',
-        cardType:'summary_large_image'
-    }
-}
+  description:
+    "I am a self-taught full stack developer, I show you my open source projects and ideas, with javascript and other technologies.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://senixcode.dev/",
+    images: [
+      {
+        url:
+          "https://raw.githubusercontent.com/senixcode/my-website/main/public/senixcode.jpg",
+        width: 800,
+        height: 600,
+        alt: "logo senixcode",
+      },
+    ],
+    site_name: "About senixcode",
+  },
+  twitter: {
+    handle: "@senixcode",
+    site: "@senixcode",
+    cardType: "summary_large_image",
+  },
+};
