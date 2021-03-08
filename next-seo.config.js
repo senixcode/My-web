e083eeac54b66ffa5e309d3881f671d9eca86818
senixcode <codesenix@gmail.com>
2021-03-07 21:49:25 -0500

    feat: :sparkles: Add images seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,6 +4,9 @@ export default {
         type:'website',
         locale:'en_US',
         url:'https://senixcode.dev/',
+         images: [
+          { url: 'https://raw.githubusercontent.com/senixcode/my-website/main/public/senixcode.jpg' },
+        ],
         site_name:'About senixcode'
     },
     twitter:{
