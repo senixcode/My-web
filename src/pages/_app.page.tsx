078b429a9969dda5422d5d613c1a0d370aa7b7ce
senixcode <codesenix@gmail.com>
2021-04-07 11:15:40 -0500

    feat: :sparkles: added config apollo client

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,42 +1,24 @@
 import { AppProps } from "next/app";
-import { DefaultSeo } from "next-seo";
-
 import { ContextWrapper } from "./ContextWrapper";
 import { ThemeProvider } from "styled-components";
 import theme from "../../styles/theme";
 import GlobalStyles from "../../styles/GlobalStyles";
-import SEO from "../../next-seo.config";
 import Header from "../components/containers/header";
+import { client } from "../graphql/config";
+import { ApolloProvider } from "@apollo/client";
+import Seo from "../components/common/Seo";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <DefaultSeo
-        {...SEO}
-        additionalMetaTags={[
-          {
-            property: "dc:creator",
-            content: "senixcode",
-          },
-          {
-            name: "senixcode",
-            content: "About me, full stack developer, open source projects.",
-          },
-          {
-            httpEquiv: "x-ua-compatible",
-            content: "IE=edge; chrome=1",
-          },
-        ]}
-        languageAlternates={[{
-           hrefLang: 'es',
-           href: 'https://www.senixcode.dev/es',
-        }]}
-      />
-      <ContextWrapper>
-        {pageProps.statusCode != 404 && <Header />}
-        <Component {...pageProps} />
-      </ContextWrapper>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Seo />
+        <ContextWrapper>
+          {pageProps.statusCode != 404 && <Header />}
+          <Component {...pageProps} />
+        </ContextWrapper>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
