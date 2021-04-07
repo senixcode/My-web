38fcead75686efa697dc3239663cad326f75bffb
senixcode <codesenix@gmail.com>
2021-04-07 11:19:23 -0500

    refactor: :recycle: change a useQuery

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,44 +1,23 @@
-import { useRouter } from "next/router";
-import { getAboutMe } from "../cmsFaker/getAboutMe";
 import { AboutMeContainer } from "../components/containers/AboutMeContainer";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { FC } from "react";
 import { IAboutMe } from "../interface/aboutMe";
+import { GET_ABOUTME } from "../graphql/querys/aboutMe";
+import { useRouter } from "next/router";
+import {  useQuery } from "@apollo/client";
 export type PropsAboutMe = {
   data: IAboutMe;
 };
-const AboutMe: FC<PropsAboutMe> = ({ data }) => {
-  return <AboutMeContainer data={data} />;
+const AboutMe: FC<PropsAboutMe> = () => {
+  const { locale } = useRouter();
+  const LANGUAGE = (locale as string).toLocaleUpperCase();
+  let { data, error, loading } = useQuery(GET_ABOUTME, {
+    variables: { param: LANGUAGE },
+  });
+  
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>Error</p>;
+  
+  return <AboutMeContainer data={data.data[0]} />;
 };
 
-export async function getStaticProps({ locale }) {
-  const LANGUAGE = (locale as string).toLocaleUpperCase();
-
-  const client = new ApolloClient({
-    // uri: "https://cms-api-website.herokuapp.com/graphql",
-    uri: "http://localhost:4000/graphql",
-    cache: new InMemoryCache(),
-  });
-  const GET_ABOUTME = gql`
-    query getAboutMeByLanguage($param: Language = ES) {
-      data: aboutMeFindByLanguage(language: $param) {
-        id
-        name
-        language
-      }
-    }
-  `;
-
-  let { data } = await client.query({
-    query: GET_ABOUTME,
-    variables: { params: LANGUAGE },
-  });
-
-  data = data.data[0];
-  return {
-    props: {
-      data,
-    },
-  };
-}
 export default AboutMe;
