84b8f7c9d9abf3c98304476095ef380fb99fdabc
senixcode <codesenix@gmail.com>
2021-04-07 11:17:05 -0500

    feat: :sparkles: added GET_ABOUTME

---
+++ src/graphql/querys/aboutMe.ts
@@ -0,0 +1,10 @@
+import { gql } from "@apollo/client";
+ export const GET_ABOUTME = gql`
+    query getByLanguage($param: Language = ES) {
+      data: aboutMeFindByLanguage(language: $param) {
+        id
+        name
+        language
+      }
+    }
+  `;
