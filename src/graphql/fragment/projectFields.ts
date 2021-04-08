307922c7da8ae615fee48ddff14fa7c98e247d2e
senixcode <codesenix@gmail.com>
2021-04-08 12:58:22 -0500

    feat: :sparkles: added PROJECT_FIELDS gql

---
+++ src/graphql/fragment/projectFields.ts
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const PROJECT_FIELDS = gql`
+  fragment project_data on Project {
+    id
+    title
+    titleSeo
+    summary
+    descriptions:descriptionParse
+    topics:topicsParse {
+      id
+      name
+    }
+    links:linksParse {
+      id
+      name
+      href
+    }
+    language
+  }
+`;
