43033f744a8e70c8d7a880c9dae227103935929e
senixcode <codesenix@gmail.com>
2021-04-08 12:59:41 -0500

    refactor: :fire: refactor use graphql

--- src/pages/projects.page.tsx
+++ src/pages/projects.page.tsx
@@ -1,8 +1,6 @@
 import { ProjectsContainer } from "../components/containers/ProjectsContainer";
-import { useRouter } from "next/router";
-import { getProjects } from "../cmsFaker/getProjects";
+
 export default function Home() {
-  let { locale } = useRouter();
-  let projects = getProjects(locale);
-  return <ProjectsContainer projects={projects} />;
+
+  return <ProjectsContainer />;
 }
