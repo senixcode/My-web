5390cda2e05a9dd8e9cb6efa45731f76b152f89e
senixcode <codesenix@gmail.com>
2021-04-08 12:56:16 -0500

    feat: :sparkles: added useQuery Project

--- src/components/organisms/ProjectContent.tsx
+++ src/components/organisms/ProjectContent.tsx
@@ -1,29 +1,45 @@
+import { useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
 import { FC } from "react";
 import styled, { css } from "styled-components";
 import { Container } from "../../../styles/system/Container";
 import { IProjects } from "../../cmsFaker/projects/interfaces";
+import { GET_PROJECT } from "../../graphql/querys/project";
 import { getResumeProject } from "../../helper/getResumeProject";
 import { PropStyleTheme } from "../../types";
+import { MiddlwareHookApolloClient } from "../common/MiddelwareHookApolloClient";
 import { ProjectCard } from "../molecules/ProjectCard";
 const Projects: FC<{ projects: Array<IProjects> }> = (props) => (
   <Container gridTemplateRowsMd={`1fr 1fr`} gridRowGap=".5em" styles={projects}>
     {props.projects.map((project) => {
-      const { description } = getResumeProject(project.summary);
-      project.summary = description;
+      // const { description } = getResumeProject(project.summary);
+      // project.summary = description;
       return <ProjectCard key={project.id} {...project} />;
     })}
   </Container>
 );
-export const ProjectContent: FC<{ projects: Array<IProjects> }> = (props) => (
-  <Container gridTemplateRowsMd={`2fr 12fr`} styles={container}>
-    {/* <Hide maxMd="none" styles={filter}>
+export const ProjectContent = () => {
+  let { locale } = useRouter();
+  const LANGUAGE = (locale as string).toLocaleUpperCase();
+  let getProject = useQuery(GET_PROJECT, {
+    variables: { param: LANGUAGE },
+  });
+  
+  const projects: Array<IProjects> = getProject.data && getProject.data.data;
+  getProject.data && console.log(getProject, projects);
+  return (
+    <Container gridTemplateRowsMd={`2fr 12fr`} styles={container}>
+      {/* <Hide maxMd="none" styles={filter}>
       <Container >
         <p>filter</p>
       </Container>
     </Hide> */}
-    <Projects projects={props.projects} />
-  </Container>
-);
+      <MiddlwareHookApolloClient {...getProject}>
+        {projects && <Projects projects={projects} />}
+      </MiddlwareHookApolloClient>
+    </Container>
+  );
+};
 const container = css`
   width: 100%;
   justify-items: start;
