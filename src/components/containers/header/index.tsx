cce1e8ca7fe25f654777f5caa95db7f24fc49bca
senixcode <codesenix@gmail.com>
2021-04-08 11:03:53 -0500

    feat: :sparkles: added useQuery route

--- src/components/containers/header/index.tsx
+++ src/components/containers/header/index.tsx
@@ -1,19 +1,37 @@
 import { MenuCellSize } from "./MenuMobile";
 import useHeader from "../../../hook/useHeader";
 import { DefaultHeader, HeaderDetails } from "./ContainerHeader";
+import { GET_ROUTE } from "../../../graphql/querys/route";
+import { useQuery } from "@apollo/client";
+import { MiddlwareHookApolloClient } from "../../common/MiddelwareHookApolloClient";
+import { IRoutes } from "../../../cmsFaker/getRoutes";
+import { RouterProps } from "../../../types";
 export default function Header() {
   const { router, menuMobileShow, headerShow, handleChangeMenu } = useHeader();
-
+  const { locale } = router;
+  const LANGUAGE = (locale as string).toLocaleUpperCase();
+  const getRoute = useQuery(GET_ROUTE, {
+    variables: { param: LANGUAGE },
+  });
+  const routes: Array<IRoutes> = getRoute.data && getRoute.data.data;
+  const propsSend = {
+    router,
+    routes,
+    handleChangeMenu,
+  };
   return (
-    <>
-      {menuMobileShow && (
-        <MenuCellSize router={router} handleChangeMenu={handleChangeMenu} />
-      )}
-      {headerShow ? (
-        <DefaultHeader router={router} handleChangeMenu={handleChangeMenu} />
+    <MiddlwareHookApolloClient {...getRoute}>
+      {menuMobileShow && getRoute.data(<MenuCellSize {...propsSend} />)}
+      {headerShow && getRoute.data ? (
+        <DefaultHeader {...propsSend} />
       ) : (
         <HeaderDetails />
       )}
-    </>
+    </MiddlwareHookApolloClient>
   );
+}
+
+export interface HeaderNavProps extends RouterProps {
+  routes?: Array<IRoutes>;
+  handleChangeMenu?: () => void;
 }
