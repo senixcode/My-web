2aa6384c543d2f9220ad1c22ab83e71ce6747df9
senixcode <codesenix@gmail.com>
2021-04-08 11:05:43 -0500

    refactor: :recycle: change type props HeaderNavProps and param routes

--- src/components/containers/header/NavSelect.tsx
+++ src/components/containers/header/NavSelect.tsx
@@ -1,17 +1,14 @@
 import Link from "next/link";
 import { FC } from "react";
-import { getRoutes, IRoutes } from "../../../cmsFaker/getRoutes";
-import { RouterProps } from "../../../types";
 import { Container } from "../../../../styles/system/Container";
 import { Hide } from "../../../../styles/system/Hide";
 import { Nav } from "../../../../styles/system/Nav";
 import { css } from "styled-components";
 import Head from "next/head";
 import { SEO_SEPARATOR } from "../../../globalVariables";
-export const NavSelect: FC<RouterProps> = ({ router }) => {
-  const { locale } = router;
-  const t: Array<IRoutes> = getRoutes(locale);
-  let { title } = t.find((route) => route.path === router.route);
+import { HeaderNavProps } from ".";
+export const NavSelect: FC<HeaderNavProps> = ({ router, routes }) => {
+  let { title } = routes.find((route) => route.path === router.route);
 
   return (
     <>
@@ -22,7 +19,7 @@ export const NavSelect: FC<RouterProps> = ({ router }) => {
       )}
       <Hide maxMd="none">
         <Container gridTemplateColumnsMd={`1fr 1fr`} styles={gridColumnGap}>
-          {t.map(
+          {routes.map(
             ({ path, title }, i) =>
               path != "/detail/[project]" && (
                 <Link href={path} key={i}>
