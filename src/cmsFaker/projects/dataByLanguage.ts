fabeefb850e83965cdf8b5876b4518e5832e41dc
senixcode <codesenix@gmail.com>
2021-04-08 12:48:00 -0500

    fix: :bug: change topics

--- src/cmsFaker/projects/dataByLanguage.ts
+++ src/cmsFaker/projects/dataByLanguage.ts
@@ -1,5 +1,9 @@
 import { ComponentType, ReactElement, ReactNode } from "react";
-import { ICON_GITHUB, ICON_LINK, ICON_FIGMA } from "../../globalVariables/icons";
+import {
+  ICON_GITHUB,
+  ICON_LINK,
+  ICON_FIGMA,
+} from "../../globalVariables/icons";
 import { ILenguage } from "../../interface/Lenguages";
 import { IProjects, RepeatPropertiesProject } from "./interfaces";
 import { SEO_GITHUB, SEO_LINK, SEO_FIGMA } from "./variables";
@@ -7,17 +11,7 @@ import { SEO_GITHUB, SEO_LINK, SEO_FIGMA } from "./variables";
 const repeatPropertiesProject: Array<RepeatPropertiesProject> = [
   {
     titleSeo: "my-website",
-    topics: [
-      "Figma",
-      "Next.js",
-      "TypeScript",
-      "yarn",
-      "Font Awesome",
-      "svg",
-      "styled-components",
-      "customized design system",
-      "Vercel",
-    ],
+    topics: [{ id: 1, name: "Figma",__typename:"Topic" }],
     links: [
       {
         nameSeo: SEO_GITHUB,
@@ -32,13 +26,14 @@ const repeatPropertiesProject: Array<RepeatPropertiesProject> = [
       {
         nameSeo: SEO_FIGMA,
         name: ICON_FIGMA,
-        link: "https://www.figma.com/file/JwPIDz1ElEbggvg38n9nyA/new-template-my-web?node-id=0%3A1",
+        link:
+          "https://www.figma.com/file/JwPIDz1ElEbggvg38n9nyA/new-template-my-web?node-id=0%3A1",
       },
     ],
   },
   {
     titleSeo: "simple-ecommerce",
-    topics: ["React","npm", "Local-storage", "Material-ui", "automic-geolocation"],
+    topics: [{ id: 1, name: "Figma",__typename:"Topic"}],
     links: [
       {
         nameSeo: SEO_GITHUB,
@@ -61,12 +56,12 @@ export const routes: ILenguage<Array<IProjects>> = {
       title: "My website",
       titleSeo: repeatPropertiesProject[0].titleSeo,
       summary:
-        'In conclusion this web will be a summary of what I want to highlight and being my letter of introduction to the world.',
+        "In conclusion this web will be a summary of what I want to highlight and being my letter of introduction to the world.",
       descriptions: [
-        'It is the combination of the professional and the personal, in this sprint I have only been able to complete the professional side.',
-        'This website will always be updated with what I acquire, both the functional part (web) and the abstract (source code). ',
+        "It is the combination of the professional and the personal, in this sprint I have only been able to complete the professional side.",
+        "This website will always be updated with what I acquire, both the functional part (web) and the abstract (source code). ",
         'It is important to mention that "senixcode" I thought of it in an experimental way, to make myself known in a different way and because it has a more personal meaning.',
-        'In conclusion this web will be a summary of what I want to highlight and being my letter of introduction to the world.'
+        "In conclusion this web will be a summary of what I want to highlight and being my letter of introduction to the world.",
       ],
       topics: repeatPropertiesProject[0].topics,
       links: repeatPropertiesProject[0].links,
@@ -90,12 +85,12 @@ export const routes: ILenguage<Array<IProjects>> = {
       title: "Mi pagina web",
       titleSeo: repeatPropertiesProject[0].titleSeo,
       summary:
-        'En conclusión esta web será un resumen de lo que quiero destacar y siendo mi carta de presentación hacia el mundo.',
+        "En conclusión esta web será un resumen de lo que quiero destacar y siendo mi carta de presentación hacia el mundo.",
       descriptions: [
-        'Es la combinación de lo profesionalmente y lo personal, en este sprint solo e podido culminar el lado profesional.',
-        'Esta web siempre estará actualizada con lo que vaya adquiriendo, tanto la parte funcional (web) y lo abstracta ( código fuente). ',
+        "Es la combinación de lo profesionalmente y lo personal, en este sprint solo e podido culminar el lado profesional.",
+        "Esta web siempre estará actualizada con lo que vaya adquiriendo, tanto la parte funcional (web) y lo abstracta ( código fuente). ",
         'Es importante mencionar que "senixcode" lo pensé de forma experimental,  para darme a conocer de forma distinta y porque tiene un significado mas personal.',
-        'En conclusión esta web será un resumen de lo que quiero destacar y siendo mi carta de presentación hacia el mundo.',
+        "En conclusión esta web será un resumen de lo que quiero destacar y siendo mi carta de presentación hacia el mundo.",
       ],
       topics: repeatPropertiesProject[0].topics,
       links: repeatPropertiesProject[0].links,
