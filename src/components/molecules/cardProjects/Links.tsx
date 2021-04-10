286039bda5b56ce34e21464766e70b90cd0f2b55
senixcode <codesenix@gmail.com>
2021-04-10 12:04:23 -0500

    refactor: :fire: delete links

--- src/components/molecules/cardProjects/Links.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import { Link } from "../../../../styles/system/Link";
-import { ILink } from "../../../cmsFaker/projects/interfaces";
-
-export const Links: FC<ILink> = ({ href , icon, name}) => (
-  <Link title={name} href={href} fontSizeMd="20px" fontSizeXs="18px">
-    <i className={icon}></i>
-  </Link>
-);
