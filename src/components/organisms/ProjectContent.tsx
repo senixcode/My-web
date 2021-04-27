2daa2d382c2435345372d01ba29dc2dc01c5d345
senixcode <codesenix@gmail.com>
2021-04-27 02:39:38 -0500

    feat: :sparkles: added see more and see less

--- src/components/organisms/ProjectContent.tsx
+++ src/components/organisms/ProjectContent.tsx
@@ -1,30 +1,53 @@
 import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
-import { FC } from "react";
+import React, { FC } from "react";
 import styled, { css } from "styled-components";
 import { Container } from "../../../styles/system/Container";
 import { IProjects } from "../../cmsFaker/projects/interfaces";
 import { GET_PROJECT } from "../../graphql/querys/project";
-import { getResumeProject } from "../../helper/getResumeProject";
 import { PropStyleTheme } from "../../types";
 import { MiddlwareHookApolloClient } from "../common/MiddelwareHookApolloClient";
 import { ProjectCard } from "../molecules/ProjectCard";
-const Projects: FC<{ projects: Array<IProjects> }> = (props) => (
+
+const Projects: FC<{
+  projects: Array<IProjects>;
+  seeMoreLang: string;
+  seeLessLang: string;
+}> = ({projects,seeLessLang,seeMoreLang}) => (
   <Container gridTemplateRowsMd={`1fr 1fr`} gridRowGap=".5em" styles={projects}>
-    {props.projects.map((project) => {
+    {projects.map((project) => {
       // const { description } = getResumeProject(project.summary);
       // project.summary = description;
-      return <ProjectCard key={project.id} {...project} />;
+      return <ProjectCard key={project.id}
+       project={project} 
+       seeMoreLang={seeMoreLang}
+       seeLessLang={seeLessLang}
+       />;
     })}
   </Container>
 );
+
 export const ProjectContent = () => {
   let { locale } = useRouter();
+  const [seeMoreLang, setSeeMoreLang] = React.useState<string>(
+    locale === "en" ? "see more" : "ver mas"
+  );
+  const [seeLessLang, setSeeLessLang] = React.useState<string>(
+    locale === "en" ? "see less" : "ver menos"
+  );
   const LANGUAGE = (locale as string).toLocaleUpperCase();
   let getProject = useQuery(GET_PROJECT, {
     variables: { param: LANGUAGE },
   });
-  
+  React.useEffect(() => {
+    if (locale) {
+      setSeeMoreLang(locale === "en" ? "see more" : "ver mas");
+      setSeeLessLang(locale === "en" ? "see less" : "ver menos");
+    }
+  }, [locale]);
+
+  console.log("aqui senixcode", getProject.data?.data);
+
   const projects: Array<IProjects> = getProject.data && getProject.data.data;
   return (
     <Container gridTemplateRowsMd={`2fr 12fr`} styles={container}>
@@ -34,7 +57,13 @@ export const ProjectContent = () => {
       </Container>
     </Hide> */}
       <MiddlwareHookApolloClient {...getProject}>
-        {projects && <Projects projects={projects} />}
+        {projects && (
+          <Projects
+            projects={projects}
+            seeMoreLang={seeMoreLang}
+            seeLessLang={seeLessLang}
+          />
+        )}
       </MiddlwareHookApolloClient>
     </Container>
   );
