5832ace7654b98be749826fcf0b68f14123ea334
senixcode <codesenix@gmail.com>
2021-05-01 22:27:42 -0500

    feat: :sparkles: added loding desing

--- src/components/common/MiddelwareHookApolloClient.tsx
+++ src/components/common/MiddelwareHookApolloClient.tsx
@@ -1,8 +1,9 @@
 import { QueryResult } from "@apollo/client";
 import { FC, Fragment } from "react";
+import Loading from "../molecules/Loading";
 
 export const MiddlwareHookApolloClient: FC<QueryResult> = (props) => {
   if (props?.error) return <p>Error {props.error.message}</p>;
-  if (props?.loading) return <p>Loading</p>;
+  if (props?.loading) return <Loading/>;
   return <Fragment>{props.children}</Fragment>;
 };
