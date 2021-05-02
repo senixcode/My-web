19b4500de109064b175b15ca026ceffd170852fa
senixcode <codesenix@gmail.com>
2021-05-02 03:52:38 -0500

    feat: :sparkles: change title seo, comment image

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,19 +1,19 @@
 export default {
   description:
-    "I am a self-taught full stack developer, I show you my open source projects and ideas, with javascript and other technologies.",
+    "Self-taught Full Stack JavaScript programmer, with experience in startups, new knowledge Docker Next Nest Graphql, looking for new challenges.",
   openGraph: {
     type: "website",
     locale: "en_US",
     url: "https://senixcode.dev/",
-    images: [
-      {
-        url:
-          "https://raw.githubusercontent.com/senixcode/my-website/main/public/senixcode.jpg",
-        width: 800,
-        height: 600,
-        alt: "logo senixcode",
-      },
-    ],
+    // images: [
+    //   {
+    //     url:
+    //       "https://raw.githubusercontent.com/senixcode/my-website/main/public/senixcode.jpg",
+    //     width: 800,
+    //     height: 600,
+    //     alt: "logo senixcode",
+    //   },
+    // ],
     site_name: "About senixcode",
   },
   twitter: {
