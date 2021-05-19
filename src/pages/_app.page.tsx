440ce79c49869befdba9b71bcdcd001958060193
senixcode <codesenix@gmail.com>
2021-05-19 10:05:42 -0500

    fix: :lipstick: show loading stylesglobal

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app"
 import { ThemeProvider } from "styled-components"
 import theme from "../../styles/theme"
-// import GlobalStyles from "../../styles/GlobalStyles"
+import GlobalStyles from "../../styles/GlobalStyles"
 import Header from "../components/containers/header"
 import Seo from "../components/common/Seo"
 import dynamic from "next/dynamic"
@@ -10,15 +10,10 @@ const DynamicContextWrapper = dynamic(
     return import("./ContextWrapper")
   },{ ssr: false }
 )
-const DynamicGlobalStyles = dynamic(
-  () => {
-    return import("../../styles/GlobalStyles")
-  },{ssr:false}
-)
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
       <ThemeProvider theme={theme}>
-        <DynamicGlobalStyles />
+        <GlobalStyles />
         <Seo />
         <DynamicContextWrapper>
           {pageProps.statusCode !== 404 && <Header />}
