c276ea384de5bd9c1e692f1c78772826e363f8e9
senixcode <codesenix@gmail.com>
2021-05-18 20:57:55 -0500

    feat: :fire: graphql

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,29 +1,29 @@
 import { AppProps } from "next/app"
 import { ThemeProvider } from "styled-components"
 import theme from "../../styles/theme"
-import GlobalStyles from "../../styles/GlobalStyles"
+// import GlobalStyles from "../../styles/GlobalStyles"
 import Header from "../components/containers/header"
-import { client } from "../graphql/config"
-import { ApolloProvider } from "@apollo/client"
 import Seo from "../components/common/Seo"
 import dynamic from "next/dynamic"
 const DynamicContextWrapper = dynamic(
   () => {
     return import("./ContextWrapper")
-  },
-  { ssr: false }
+  },{ ssr: false }
+)
+const DynamicGlobalStyles = dynamic(
+  () => {
+    return import("../../styles/GlobalStyles")
+  },{ssr:false}
 )
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
-        <GlobalStyles />
+        <DynamicGlobalStyles />
         <Seo />
         <DynamicContextWrapper>
           {pageProps.statusCode !== 404 && <Header />}
           <Component {...pageProps} />
         </DynamicContextWrapper>
       </ThemeProvider>
-    </ApolloProvider>
   )
 }
--- src/types/enums/LinkCategory.ts
+++
@@ -1,5 +0,0 @@
-export enum LinkCategory {
-  VIDEO = "VIDEO",
-  IMAGE = "IMAGE",
-  ICON = "ICON",
-}
